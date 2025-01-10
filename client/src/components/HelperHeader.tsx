203ea904869dc8f6b1b6bea8f17e2ae8756bf6d5
Deepansh Agarwal <deepansh.agarwal.2234@gmail.com>
2025-01-10 23:47:40 +0530

    Add loadCode functionality and update dependencies for improved code management

--- client/src/components/HelperHeader.tsx
+++ client/src/components/HelperHeader.tsx
@@ -1,12 +1,6 @@
 import { Button } from "./ui/button";
-import { Save, Share2Icon } from "lucide-react";
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select";
+import { Code, Copy, Loader, Save, Share2Icon } from "lucide-react";
+
 import { useDispatch, useSelector } from "react-redux";
 import {
     CompilerSliceStateType,
@@ -15,21 +9,57 @@ import {
 import { RootState } from "../redux/store";
 import { handleError } from "../utils/handleError";
 import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "./ui/dialog";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "./ui/select";
+import { toast } from "sonner";
 
 const HelperHeader = () => {
+    const [saveLoading, setSaveLoading] = useState<boolean>(false);
+    const [shareBtn, setShareBtn] = useState<boolean>(false);
+    const { urlId } = useParams();
+
+    const navigate = useNavigate();
+
     const fullCode = useSelector(
         (state: RootState) => state.compilerSlice.fullCode
     );
 
+    useEffect(() => {
+        if (urlId) {
+            setShareBtn(true);
+        } else {
+            setShareBtn(false);
+        }
+    }, [urlId]);
+
     const handleSaveCode = async () => {
+        setSaveLoading(true);
         try {
             const response = await axios.post(
                 "http://localhost:4000/compiler/save",
                 { fullCode: fullCode }
             );
             console.log(response.data);
+            navigate(`/compiler/${response.data.url}`, { replace: true });
         } catch (error) {
             handleError(error);
+        } finally {
+            setSaveLoading(false);
         }
     };
 
@@ -44,17 +74,63 @@ const HelperHeader = () => {
                     onClick={handleSaveCode}
                     className="flex justify-center items-center gap-1"
                     variant={"success"}
+                    disabled={saveLoading}
                 >
-                    <Save />
-                    Save
+                    {saveLoading ? (
+                        <>
+                            <Loader className="animate-spin" />
+                        </>
+                    ) : (
+                        <>
+                            <Save />
+                            Save
+                        </>
+                    )}
                 </Button>
-                <Button
-                    className="flex justify-center items-center gap-1"
-                    variant={"secondary"}
-                >
-                    <Share2Icon />
-                    Share
-                </Button>
+                {shareBtn && (
+                    <Dialog>
+                        <DialogTrigger className="whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9 px-4 py-2 flex justify-center items-center gap-1">
+                            <Share2Icon />
+                            Share
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle className="flex justify-center items-center gap-1">
+                                    <Code size={18} />
+                                    Share your Code!
+                                </DialogTitle>
+                                <DialogDescription className="flex flex-col gap-2">
+                                    <div className="__url flex gap-1">
+                                        <input
+                                            type="text"
+                                            disabled
+                                            className="w-full px-2 py-2 rounded text-slate-400 bg-slate-800 text-center"
+                                            value={window.location.href}
+                                        />
+                                        <Button
+                                            variant={"outline"}
+                                            onClick={() => {
+                                                window.navigator.clipboard.writeText(
+                                                    window.location.href
+                                                );
+                                                toast(
+                                                    "URL copied to clipboard"
+                                                );
+                                            }}
+                                        >
+                                            <Copy />
+                                        </Button>
+                                    </div>
+
+                                    <p className="text-center">
+                                        Share this URL with your friends to
+                                        collaborate.
+                                    </p>
+                                </DialogDescription>
+                            </DialogHeader>
+                        </DialogContent>
+                    </Dialog>
+                )}
             </div>
 
             <div className="__tab_switcher flex justify-center items-center gap-1">
--- client/src/pages/Compiler.tsx
+++ client/src/pages/Compiler.tsx
@@ -1,5 +1,6 @@
 // import React from "react";
 
+import { useParams } from "react-router-dom";
 import CodeEditor from "../components/CodeEditor";
 import HelperHeader from "../components/HelperHeader";
 import RenderCode from "../components/RenderCode";
@@ -8,8 +9,41 @@ import {
     ResizablePanel,
     ResizablePanelGroup,
 } from "../components/ui/resizable";
+import { useEffect } from "react";
+import axios from "axios";
+import { handleError } from "../utils/handleError";
+import { useDispatch } from "react-redux";
+import { updateFullCode } from "../redux/slices/compilerSlice";
+import { toast } from "sonner";
 
 const Compiler = () => {
+    const { urlId } = useParams();
+    const dispatch = useDispatch();
+
+    const loadCode = async () => {
+        try {
+            const response = await axios.post(
+                "http://localhost:4000/compiler/load",
+                {
+                    urlId: urlId,
+                }
+            );
+            dispatch(updateFullCode(response.data.fullCode));
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                if (error?.response?.status === 500) {
+                    toast("Invalid URL, Default Code Loaded");
+                }
+            }
+            handleError(error);
+        }
+    };
+
+    useEffect(() => {
+        if (urlId) {
+            loadCode();
+        }
+    }, [urlId]);
     return (
         <ResizablePanelGroup direction="horizontal">
             <ResizablePanel
