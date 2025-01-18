6837dbe1db5769ab1b92e9c4267cbef448caf5c8
Deepansh Agarwal <deepansh.agarwal.2234@gmail.com>
2025-01-19 03:41:53 +0530

    config cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,13 @@ import { compilerRouter } from "./routes/compiler.routes";
 const PORT = process.env.PORT || 4000;
 const app: Application = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: "*", // Allow all origins. You can restrict this to specific origins if needed.
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 dbConnect();
 
