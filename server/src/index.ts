ecb1f2277b1f1d15b6d001327881971b496197b8
Deepansh Agarwal <deepansh.agarwal.2234@gmail.com>
2025-01-19 03:16:41 +0530

    Refactor server initialization to enhance environment variable usage and add a root endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,23 @@
-import express, { Application } from "express";
 import cors from "cors";
-import { config } from "dotenv";
+import express, { Application } from "express";
+import "dotenv/config";
+
 import { dbConnect } from "./lib/dbConnect";
 import { compilerRouter } from "./routes/compiler.routes";
 
 const PORT = process.env.PORT || 4000;
 const app: Application = express();
 
-app.use(express.json());
 app.use(cors());
-config();
+app.use(express.json());
+dbConnect();
 
 app.use("/compiler", compilerRouter);
 
-dbConnect();
+app.get("/", (req, res) => {
+    res.send("API Working");
+});
+
 app.listen(PORT, () => {
     console.log("Server listening on PORT: " + PORT);
 });
