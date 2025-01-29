4c074a1beb55d2aae9f23e908d6262c6e024702c
Deepansh Agarwal <deepansh.agarwal.2234@gmail.com>
2025-01-29 15:39:51 +0530

    Working on local host

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,15 +8,17 @@ import { compilerRouter } from "./routes/compiler.routes";
 const PORT = process.env.PORT || 4000;
 const app: Application = express();
 
-const allowedOrigins = ["https://online-web-dev-compiler.vercel.app"]; // Add your frontend origin
+// const allowedOrigins = ["https://online-web-dev-compiler.vercel.app","localhost:5173"]; // Add your frontend origin
 
-app.use(
-    cors({
-        origin: allowedOrigins,
-        methods: ["GET", "POST", "PUT", "DELETE"], // Add allowed HTTP methods
-        credentials: true, // Enable if using cookies or authentication
-    })
-);
+// app.use(
+//     cors({
+//         origin: allowedOrigins,
+//         methods: ["GET", "POST", "PUT", "DELETE"], // Add allowed HTTP methods
+//         credentials: true, // Enable if using cookies or authentication
+//     })
+// );
+
+app.use(cors());
 
 app.use(express.json());
 dbConnect();
